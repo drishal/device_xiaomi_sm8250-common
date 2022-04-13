142a8b6df90a7ab9f3289be62f43ac828ae02ad3
drishal <drishalballaney@gmail.com>
2022-04-13 15:26:33 +0530

    Revert "sm8250-common: USB: Finalize upver to android.hardware.usb@1.3"
    This reverts commit 1756c8ab2351a6947a5b39d947e8d3dbd3d95950.

--- kona.mk
+++ kona.mk
@@ -721,7 +721,7 @@ endif
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.2-service-qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
